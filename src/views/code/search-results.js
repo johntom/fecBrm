32f2cf57cbfe0319fbbd9f9fcd5980c1d14c7692
John Tomaselli <jrt@gtz.com>
2018-05-09 14:44:43 -0400

    Auto commit from FrontEnd Creator

--- src/views/code/search-results.js
+++ src/views/code/search-results.js
@@ -45,8 +45,8 @@ export class SearchResults {
             }
             this.dataSource.read()
           })
-         options.success()
-       },
+        options.success()
+      },
     },
     schema: {
       model: {
@@ -63,6 +63,69 @@ export class SearchResults {
   })
 
 
+  dataSource = new kendo.data.DataSource({
+    transport: {
+      read: (options) => {
+        //  this.loadData(this.capColor, this.prevtown)
+        this.loadData()
+          .then((code) => {
+            // console.log('   scans ', scans[0])
+            options.success(code)
+
+          })
+
+      },
+
+      destroy: (options) => {
+
+        let updatedItem = options.data;
+        console.log('   updatedItem ', updatedItem)
+        this.deleteData(updatedItem)
+          .then((scans) => {
+            options.success(scans)
+            this.dataSource.read()
+          })
+        options.success()
+      },
+      update: (options) => {
+     
+        let updatedItem = options.data;
+        console.log('   updatedItem ', updatedItem)
+        this.updateData(updatedItem)
+          .then((code) => {
+            options.success(code)
+            // if (code.data === 'alreadyComplete') {
+            //   alert('record was completed no updates allowed...')
+            //   //   this.toast.show('record was completed no updates allowed!', 4000);
+            // }
+            this.dataSource.read()
+          })
+        options.success()
+      }
+    },
+    filter: { field: "inactive", operator: "eq", value: false },
+
+    schema: {
+      model: {
+        id: "id", // Must assign id for update to work
+        // id: "_id", // if useing native then change id to _id        
+        fields: {
+
+          inactive: {
+            type: 'boolean'
+          },
+        }
+      }
+    },
+    pageSize: 10,
+    sort: { field: 'filename', dir: 'asc' },
+
+  })
+
+
+
+
+
   constructor(router, api, utilService, appService, dataService) {
     this.router = router;
     this.api = api;
@@ -74,10 +137,10 @@ export class SearchResults {
     console.log('in updateData ', e)
     return [{ data: 'all' }]
     return this.api.updateccode(e)
-        .then((jsonRes) => {
-            console.log('this.scans ', jsonRes)
-            return jsonRes
-        })
+      .then((jsonRes) => {
+        console.log('this.scans ', jsonRes)
+        return jsonRes
+      })
   }
   activate(params, routeConfig) {
     //http://74.114.164.24/api/v1/inventorycontent?artistl=s%26artistf=c 
