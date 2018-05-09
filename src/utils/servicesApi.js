49ea4146e3348ecc8cc22e3b3f908b6d5d3faaec
John Tomaselli <jrt@gtz.com>
2018-05-09 12:42:44 -0400

    Auto commit from FrontEnd Creator

--- src/utils/servicesApi.js
+++ src/utils/servicesApi.js
@@ -8,7 +8,8 @@ export class ApiService {
     this.http = http;
     this.upmess = ''
     //this.baseweb = 'http://localhost:8080/api/';
-    this.baseweb = 'https://jif.bergenrisk.com:8081/api/';
+    // this.baseweb = 'https://jif.bergenrisk.com:8081/api/';
+    this.baseweb = 'https://jif.bergenrisk.com/api/';
   }
   getUserJwt(username, pass) {
     var token = {};
