d7f6f7af110a2234fc593fd919b6b21df9bf598b
John Tomaselli <jrt@gtz.com>
2018-05-17 07:45:52 -0400

    Auto commit from FrontEnd Creator

--- src/views/claim/data-form.js
+++ src/views/claim/data-form.js
@@ -14,7 +14,15 @@ export class DataForm {
   footer = 'DataAddForm FOOTER...';
   adjusterList = 'adjusterList';
   recordId = '';
-  //inscontactMatcher='';
+   products = [
+        { id: 0, name: 'Motherboard' },
+        { id: 1, name: 'CPU' },
+        { id: 2, name: 'Memory' },
+      ];
+
+      productMatcher = (a, b) => a.id === b.id;
+
+      selectedProduct = { id: 1, name: 'CPU' };
 
   constructor(router, api, appService, dataService, eventAggregator, dialogService) {
     this.api = api;
