ae77bee6b646cd4fb6e08754f1ef53b0e4a81738
John Tomaselli <jrt@gtz.com>
2018-05-17 14:49:26 -0400

    Auto commit from FrontEnd Creator

--- src/views/claim/data-form.js
+++ src/views/claim/data-form.js
@@ -6,7 +6,7 @@ import { ApplicationService } from '../../services/application-service';
 import { MyDataService } from "../../services/my-data-service";
 import { EventAggregator } from 'aurelia-event-aggregator';
 import { Prompt } from './prompt';
-import { Promptyn } from '../../services/promptyn';
+import { Promptyn } from './promptyn';
 import { DialogService } from 'aurelia-dialog';
 import { lodash } from 'lodash'
 @inject(Router, ApiService, ApplicationService, MyDataService, EventAggregator, DialogService)
