3ee89207268090179962a5f7f71bf16872afff04
John Tomaselli <jrt@gtz.com>
2018-05-18 11:58:20 -0400

    105

--- src/views/claim/data-form.js
+++ src/views/claim/data-form.js
@@ -311,7 +311,7 @@ export class DataForm {
     // // Next, we navigate to the newly created claim
     // // Finally, we close out this tab
     // this.closeTab(tab);
-    //let rt2 = '#/claim/' + this.tabname ///claim'//Search?'cant use when search has a number 
+    let rt2 = '#/claim/' + this.tabname ///claim'//Search?'cant use when search has a number 
     //console.log('this.tabname ', this.tabname)
     this.router.navigate(rt2);
 
