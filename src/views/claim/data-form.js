90e02eefcef2bcd4df083237ef4b2dfdad67ff1d
John Tomaselli <jrt@gtz.com>
2018-05-29 11:25:03 -0400

    v131L

--- src/views/claim/data-form.js
+++ src/views/claim/data-form.js
@@ -83,10 +83,10 @@ export class DataForm {
           console.log('jsonRes ', jsonRes);
           let claim = jsonRes.data
           console.log('claiminv ', claim);
-          this.appService.currentClaim = claim[0];
-          //this.appService.originalrec =  claim[0]
+         // this.appService.currentClaim = claim[0];
+         //this.appService.originalrec =  claim[0]
        
-        this.appService.currentClaim = JSON.parse(JSON.stringify(claim[0]));
+         this.appService.currentClaim = JSON.parse(JSON.stringify(claim[0]));
         
           this.appService.currentClaim.isDirty = () => {
        //     return JSON.stringify(this.appService.currentClaim) !== JSON.stringify(this.appService.originalrec)
