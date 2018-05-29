be31df97cf77d853d6605fd82fcd9094a73bcda6
John Tomaselli <jrt@gtz.com>
2018-05-29 16:47:53 -0400

    v131f

--- src/views/claim/data-form.js
+++ src/views/claim/data-form.js
@@ -85,9 +85,9 @@ attached(){
         return this.api.findclaimOne(this.recordId).then((jsonRes) => {
           // console.log('jsonRes ', jsonRes);
           let claim = jsonRes.data
-          this.appService.currentClaim = {}
+          // this.appService.currentClaim = {}
           this.appService.currentClaim = claim[0];
-        
+          this.lossdesc = appService.currentClaim.DESCRIPTION
           console.log('claim[0] ', this.appService.currentClaim.DESCRIPTION, claim[0]);
           // cant do this.appService.currentClaim = JSON.stringify( claim[0])
           //this.appService.originalrec =  claim[0]
