4677936c4cb5c35da084654351fbd661723a68d4
John Tomaselli <jrt@gtz.com>
2018-06-13 13:27:49 -0400

    v0613p

--- src/views/claim/data-form.js
+++ src/views/claim/data-form.js
@@ -132,7 +132,7 @@ export class DataForm {
           this.appService.testrec = claim[0];
           this.currentItem.isDirty = () => {
             //return JSON.stringify(this.currentItem) !== JSON.stringify(this.appService.originalrec)
-            return this.comparedata()
+            return !this.comparedata()
           };
           this.currentItem.reset = () => {
             this.appService.originalrec = this.currentItem;
