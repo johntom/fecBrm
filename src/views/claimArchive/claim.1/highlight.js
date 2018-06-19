396b6aac188565aafad636df41a1becc9090da35
John Tomaselli <jrt@gtz.com>
2018-06-19 08:54:41 -0400

    Removed src/views/claimArchive/claim.1/highlight.js

--- src/views/claimArchive/claim.1/highlight.js
+++
@@ -1,8 +0,0 @@
-export class HighlightValueConverter{
-    toView(value, searchTerm) {
-      let name = value.name;
-    console.log('value name',name)
-       if(!searchTerm) return name;
-       return name.replace(new RegExp(searchTerm, 'gi'), `<b>$&</b>`);
-    }
-}
