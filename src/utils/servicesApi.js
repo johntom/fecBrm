9ad33ebef3907bbb4941cbd1d9096b1535b8b7ca
John Tomaselli <jrt@gtz.com>
2018-07-13 18:07:19 -0400

    fri

--- src/utils/servicesApi.js
+++ src/utils/servicesApi.js
@@ -7,8 +7,8 @@ export class ApiService {
   constructor(http) {
     this.http = http;
     this.upmess = ''
-    this.baseweb = 'https://gtztest.com/api/'
-    // this.baseweb = 'https://jif.bergenrisk.com/api/';
+    //this.baseweb = 'https://gtztest.com/api/'
+    this.baseweb = 'https://jif.bergenrisk.com/api/';
   }
   getUserJwt(username, pass) {
     var token = {};
