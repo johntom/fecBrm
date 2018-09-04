6e763b676952467ecb76ded3b5f341413789db8c
John Tomaselli <jrt@gtz.com>
2018-09-04 13:02:36 -0400

    100.17

--- src/views/claim/prompt.js
+++ src/views/claim/prompt.js
@@ -219,7 +219,20 @@ export class Prompt {
   static inject = [DialogController, ApplicationService, MyDataService, DialogService, ApiService];
 
 fruits = ['Apple', 'Orange', 'Grapes', 'Pineaple', 'Peach', 'Bananas'];
-
+monthsOfTheYear = [
+    {name: 'January', short: 'Jan', number: 1},
+    {name: 'February', short: 'Feb', number: 2},
+    {name: 'March', short: 'Mar', number: 3},
+    {name: 'April', short: 'Apr', number: 4},
+    {name: 'May', short: 'May', number: 5},
+    {name: 'June', short: 'Jun', number: 6},
+    {name: 'July', short: 'Jul', number: 7},
+    {name: 'August', short: 'Aug', number: 8},
+    {name: 'September', short: 'Sep', number: 9},
+    {name: 'October', short: 'Oct', number: 10},
+    {name: 'November', short: 'Nov', number: 11},
+    {name: 'December', short: 'Dec', number: 12}
+  ];
   constructor(controller, appService, dataService, dialogService, api) {
     this.controller = controller;
     this.answer = null;
@@ -262,7 +275,13 @@ fruits = ['Apple', 'Orange', 'Grapes', 'Pineaple', 'Peach', 'Bananas'];
 testfruit(){
   alert(this.fruit+' this.fn' +this.FullName)
 }
- 
+   monthSelected(item) {
+    if (item) {
+      console.log('Month Selected: ' + item.short);
+    } else {
+      console.log('Month cleared');
+    }
+  }
   attached() {
        // set typahead value for state MUST BE IN ATTACHED
     // this.name = {
