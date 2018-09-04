c69e8479aa332459651b3ce3640486d621880d9c
John Tomaselli <jrt@gtz.com>
2018-09-04 12:01:29 -0400

    100.11

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -8,7 +8,7 @@ import { MyDataService } from "../../services/my-data-service";
 
 export class Home {
   heading = "Welcome to the Home page!";
-  version = 100.10
+  version = 100.11
 
   constructor(appService, dataService) {
     //    if (appService.claimLookupDataLoaded) {
